7cfed0a708a6f59610638b4e691bf31827633846
Nero-z17 <ryanowafeu5@gmail.com>
2026-06-08 17:46:55 +0100

    maj folder style

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom'; // 1. Import important
 import App from './App';
 import { ThemeProvider } from './context/ThemeContext';
-import './sTyles/index.css';
+import './styles/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
