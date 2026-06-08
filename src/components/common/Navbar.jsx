723c18214b2666078d28e69c1633ddcccfa0c486
Nero-z17 <ryanowafeu5@gmail.com>
2026-06-08 20:36:04 +0100

    logo

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -24,7 +24,7 @@ const Navbar = () => {
   return (
     <nav className="fixed top-0 left-0 w-full z-50 bg-blue-500 dark:bg-gray-950/80 backdrop-blur-md border-b border-gray-200 dark:border-gray-800 transition-colors duration-300">
       <div className="max-w-7xl mx-auto px-4 py-3 flex justify-between items-center">
-        <h1 className="font-bold text-xl text-gray-200 dark:text-white">Streaming ERP</h1>
+        <h1 className="font-bold text-xl text-gray-200 dark:text-white">Nero ERP</h1>
         
         {/* Navigation Desktop - Hidden sur mobile */}
         <div className="hidden md:flex items-center space-x-4">
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,5 +9,32 @@ export default defineConfig({
     host: true,
     port: 5173
   },
-  plugins: [react(),tailwindcss(),],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate', // Met à jour l'app automatiquement si tu pousses du nouveau code
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      manifest: {
+        name: 'Nero ERP', // Le nom complet
+        short_name: 'Nero ERP', // Le nom sous le logo sur l'écran du tel
+        description: 'Gestion professionnelle des abonnements streaming',
+        theme_color: '#000000', // La couleur de la barre de statut du tel (Noir profond pour ton thème)
+        background_color: '#000000', // La couleur d'ouverture de l'app
+        display: 'standalone', // C'est ça qui enlève la barre de recherche (Mode App)
+        icons: [
+          {
+            src: './public/logo1.png',
+            sizes: '804x838',
+            type: 'image/png'
+          },
+          {
+            src: './public/logo2.png',
+            sizes: '377x382',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
 })
